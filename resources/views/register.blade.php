faedacad452fb11f7fdc6e87bda9a89c4e738acc
agusyudia <smkagus8@gmail.com>
2022-06-07 14:45:14 +0800

    login done

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Register</title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="container"><br>
-        <div class="col-md-4 col-md-offset-4">
-            <h2 class="text-center"><b>MIRACLE</b></h3>
+        <div class="col-md-4 mx-auto">
+            <h2 class="text-center"><b>MIRACLE</b><br>Register Account</h3>
                 <hr>
                 @if(session('error'))
                 <div class="alert alert-danger">
@@ -21,19 +21,21 @@
                 @endif
                 <form action="{{ route('actionlogin') }}" method="post">
                     @csrf
-                    <div class="form-group">
+                    <div class="form-group mt-2">
                         <label>Name</label>
                         <input type="text" name="name" class="form-control" placeholder="Name" required="">
                     </div>
-                    <div class="form-group">
+                    <div class="form-group mt-2">
                         <label>Email</label>
                         <input type="email" name="email" class="form-control" placeholder="Email" required="">
                     </div>
-                    <div class="form-group">
+                    <div class="form-group mt-2">
                         <label>Password</label>
                         <input type="password" name="password" class="form-control" placeholder="Password" required="">
                     </div>
-                    <button type="submit" class="btn btn-primary btn-block">Register</button>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary btn-block mt-2 text-center">Register</button>
+                    </div>
                     <hr>
                     <p class="text-center">Sudah punya akun? <a href="{{route('login')}}">Login</a> sekarang!</p>
                 </form>
